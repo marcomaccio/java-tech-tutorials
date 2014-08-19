1c46ff6ed969481f63ab783df0a1a49b940ba86b
Marco Maccio <marco.maccio@marmac.name>
2014-08-19 14:32:39 +0200

    Added the Cucumber test CRUD REST API of CXF-RS
    Added the base structure to develop the Cucumber test against the
    CXF-RS example of a Customer CRUD REST SERVICE

---
+++ cxf/cxf-rs/src/main/java/name/marmac/tutorials/java/cxf/services/web/rest/clients/webclient/CustomerProvisioningWebClient.java
@@ -0,0 +1,48 @@
+package name.marmac.tutorials.java.cxf.services.web.rest.clients.webclient;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import name.marmac.tutorials.java.cxf.model.to.customers.CustomersTOType;
+import name.marmac.tutorials.java.cxf.model.to.customers.ObjectFactory;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by marcomaccio on 13/08/2014.
+ */
+
+public class CustomerProvisioningWebClient {
+
+    private static final transient Logger LOGGER = LoggerFactory.getLogger(CustomerProvisioningWebClient.class);
+
+    private ObjectFactory   mCustomersTOTypeObjectFactory;
+    private WebClient       mCxfWebClient = null;
+
+
+    public CustomerProvisioningWebClient(String url){
+        //Set up the JAXB ObjectFactory to create the Java Classes to serialize/deserialize the xml
+        mCustomersTOTypeObjectFactory = new ObjectFactory();
+        //Set up the cxf WebClient
+        List<Object> providers = new ArrayList<Object>();
+        providers.add( new JacksonJaxbJsonProvider() );
+        mCxfWebClient = WebClient.create(url, providers);
+    }
+
+
+    public CustomersTOType createCustomers(CustomersTOType customersTOType) {
+        Response r = mCxfWebClient.accept("application/json").type("application/json").post(customersTOType);
+        LOGGER.info("Response status: " + r.getStatus());
+
+        return customersTOType;
+    }
+
+    public CustomersTOType getCustomers() {
+        CustomersTOType customers = mCxfWebClient.accept("application/json").path("/customers").get(CustomersTOType.class);
+
+        return customers;
+    }
+}
