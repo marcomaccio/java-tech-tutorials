aed18c7aa12084ffa3dbac0747fe5bbee2d1f696
Marco Maccio <marco.maccio@marmac.name>
2016-01-05 14:17:54 +0100

    solving issue on the test

--- cxf/cxf-rs/src/test/java/name/marmac/tutorials/java/cxf/services/web/rest/tests/clients/proxy/ProxyClientTest.java
+++ cxf/cxf-rs/src/test/java/name/marmac/tutorials/java/cxf/services/web/rest/tests/clients/proxy/ProxyClientTest.java
@@ -15,6 +15,6 @@ public class ProxyClientTest extends AbstractProxyClient {
     @Test
     public void testGetCustomerByQuery(){
         CustomersTOType customersByQuery = customerProvisioningProxyService.getCustomersByQuery(0,0,null);
-        Assert.assertEquals(4L, customersByQuery.getTotalRecords());
+        Assert.assertEquals(2L, customersByQuery.getTotalRecords());
     }
 }
